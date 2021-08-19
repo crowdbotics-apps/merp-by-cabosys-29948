a68d942f44bb4b1de3914dbb7f0d2875417d2bcb
Crowdbotics <team@crowdbotics.com>
2021-08-19 17:29:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/merp_by_cabosys_29948/MainActivity.java
@@ -0,0 +1,15 @@
+package com.merp_by_cabosys_29948;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "merp_by_cabosys_29948";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "merp_by_cabosys_29948Identifier",
+  url: "https://merp_by_cabosys_29948Identifier.botics.co",
+  api: "https://merp_by_cabosys_29948Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
